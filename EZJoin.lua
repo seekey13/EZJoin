f27d1cdc7f955e7b9b11900dd5efded524cbe5bb
Seekey13 <dcichy@gmail.com>
2026-01-09 11:43:22 -1000

    OR invites you to join a party

--- EZJoin.lua
+++ EZJoin.lua
@@ -9,7 +9,7 @@ This addon is designed for Ashita v4.
 
 addon.name      = 'EZJoin';
 addon.author    = 'Seekey';
-addon.version   = '0.1';
+addon.version   = '0.2';
 addon.desc      = 'Automatically joins party requests.';
 addon.link      = 'https://github.com/seekey13/EZJoin';
 
@@ -18,7 +18,7 @@ require('common');
 -- Event: Incoming text (chat messages)
 ashita.events.register('text_in', 'text_in_cb', function (e)
     -- Check if the message contains the invisibility block message
-    if string.find(e.message, 'You have received a request to join your party from') then
+    if string.find(e.message, 'You have received a request to join your party from') or string.find(e.message, 'invites you to join a party') then
         -- Issue the command to remove invisible buff (buff ID 69)
         AshitaCore:GetChatManager():QueueCommand(1, '/join');
     end
